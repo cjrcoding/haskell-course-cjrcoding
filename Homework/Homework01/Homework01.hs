93a8cd1bf29bc7f480e3e975d54391091a55e05a
iconcoding <rivera.carlos91@gmail.com>
2023-01-02 16:12:46 +0000

    -- HOMEWORK01

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -13,10 +13,13 @@
     f x = x*3
 -- Question 3
 -- Define a function that calculates the area of a circle.
+    areaCircle r = pi * r^2
 
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+    volCylinder h r = h * areaCircle r
 
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
+    bigVolume h r = volCylinder h r >= 42
 
