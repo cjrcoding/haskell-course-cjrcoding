27ca71a6ed11395da7f7ba4334aa2cd1c606828f
CJ <75657932+cjrcoding@users.noreply.github.com>
2023-01-02 15:07:34 +0000

    testing

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -9,7 +9,8 @@
 
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
-    func x = x*3
+    f :: Int -> Int 
+    f x = x*3
 -- Question 3
 -- Define a function that calculates the area of a circle.
 
