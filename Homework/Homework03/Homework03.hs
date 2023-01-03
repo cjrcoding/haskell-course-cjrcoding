370d3b64f2113e6c17b1aaadbe1778b4e0c7e445
iconcoding <rivera.carlos91@gmail.com>
2023-01-03 14:58:40 +0000

    Homework03  Q 1+2 merge, add Q3 answer

--- Homework/Homework03/Homework03.hs
+++ Homework/Homework03/Homework03.hs
@@ -2,13 +2,15 @@
 -- Write a function that checks if the monthly consumption of an electrical device is bigger, equal, or smaller than the maximum allowed and
 -- returns a message accordingly.
 
-    checkConsumption :: (Ord a, Show a, Num a) => a -> a -> a -> [Char]
-    checkConsumption consumption time maxC
-        | monthlyC > maxC = "Penguins are burning!"  
-        | monthlyC == maxC = "Stop Right there, you are ok!"
-        | otherwise = "we need more data!"
-      where
-          monthlyC = consumption * time * 30
+
+checkConsumption :: (Ord a, Show a, Num a) => a -> a -> a -> [Char]
+checkConsumption consumption time maxC
+    | monthlyC > maxC = "Penguins don't liek you: " ++ show (monthlyC - maxC) ++ "kWh per month!"
+    | monthlyC == maxC = "This is as far as you go!"
+    | otherwise = "You're saving: " ++ show (maxC - monthlyC) ++ "kWh per month! Good job!!"
+    where
+        monthlyC = consumption * time * 30
+
 
 
 -- Question 2
@@ -18,16 +20,21 @@
 
 -- In the previous function, return the excess/savings of consumption as part of the message.
 
-checkConsumption consumption time maxC
-    | monthlyC > maxC = "Penguins don't liek you: " ++ show (monthlyC - maxC) ++ "kWh per month!"
-    | monthlyC == maxC = "This is as far as you go!"
-    | otherwise = "You're saving: " ++ show (maxC - monthlyC) ++ "kWh per month! Good job!!"
-    where
-        monthlyC = consumption * time * 30
+-- Question 1 + 2 answered in the function above
+
+
 
 -- Question 3
 -- Write a function that showcases the advantages of using let expressions to split a big expression into smaller ones.
 -- Then, share it with other students in Canvas.
+
+bigExpression :: Double -> Double
+bigExpression a =
+  let b = a + 1
+      c = b * 2
+      d = c ^ 2
+  in d - b
+
 
 
 -- Question 4
